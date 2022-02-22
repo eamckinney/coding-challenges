ce03d69d207f6d13b371727545ab53d50abadb68
Emily McKinney <emily.a.mckinney@gmail.com>
2022-02-22 10:27:08 -0600

    extra number alt solutions

--- CodeSignal/The Core > At the Crossroads/11. Extra Number.js
+++ CodeSignal/The Core > At the Crossroads/11. Extra Number.js
@@ -1,6 +1,8 @@
 /* You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the third integer?*/
 
 
+/* MY SOLUTION */
+
 function solution(a, b, c) {
   return Math.max(
       a == b && c,
@@ -8,3 +10,14 @@ function solution(a, b, c) {
       a == c && b
   );
 }
+
+/* Solution using XOR Bitwise Operator */
+
+function solution(a, b, c) {
+  return (a^b)^c
+}
+
+
+/* Solution using chaining ternary operators */
+
+solution =(a, b, c) => a==b? c: a==c? b:a
